35bebceba72a750d1b76c3cffda01afe99a03d26
hsh <bajpaiharsh244@gmail.com>
2023-07-27 15:01:44 +0530

    fix: fix workflow file

--- benchmarking/scripts/run_rpc.sh
+++ benchmarking/scripts/run_rpc.sh
@@ -15,9 +15,16 @@ KAKAROT_ADDRESS=$(jq -r '.kakarot.address' ../lib/kakarot/deployments/madara/dep
 echo "starting RPC with the KAKAROT_ADDRESS=$KAKAROT_ADDRESS"
 
 # run rpc with environment variables
+# KAKAROT_HTTP_RPC_ADDRESS=0.0.0.0:3030 \
+# PROXY_ACCOUNT_CLASS_HASH=0x4b9eef81a3f0a582dfed69be93196cedbff063e0fa206b34b4c2f06ac505f0c \
+# RUST_LOG=error \
+# STARKNET_NETWORK=madara \
+# KAKAROT_ADDRESS=$KAKAROT_ADDRESS \
+# ../target/release/kakarot-rpc
+
 KAKAROT_HTTP_RPC_ADDRESS=0.0.0.0:3030 \
 PROXY_ACCOUNT_CLASS_HASH=0x4b9eef81a3f0a582dfed69be93196cedbff063e0fa206b34b4c2f06ac505f0c \
 RUST_LOG=error \
 STARKNET_NETWORK=madara \
 KAKAROT_ADDRESS=$KAKAROT_ADDRESS \
-../target/release/kakarot-rpc
+../lib/bin/kakarot-rpc
