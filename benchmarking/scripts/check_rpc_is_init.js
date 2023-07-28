89df7fcf461e6c34dc23c490659002084ab36a1f
bajpai244 <bajpaiharsh244@gmail.com>
2023-07-28 22:05:37 +0530

    dev: add prettier file

--- benchmarking/scripts/check_rpc_is_init.js
+++ benchmarking/scripts/check_rpc_is_init.js
@@ -1,9 +1,8 @@
-const {waitForRPCInit} = require("./utils")
+const { waitForRPCInit } = require("./utils");
 
-const main = async () =>  { 
-    console.log("checking RPC is up and running ...")
-    await waitForRPCInit(100)
-}
+const main = async () => {
+  console.log("checking RPC is up and running ...");
+  await waitForRPCInit(100);
+};
 
-main()
-
+main();
--- benchmarking/scripts/processor.js
+++ benchmarking/scripts/processor.js
@@ -1,59 +1,47 @@
 const { providers, Wallet } = require("ethers");
-const { parseEther} = require("ethers/lib/utils");
-const {sleep} = require("./utils")
+const { parseEther } = require("ethers/lib/utils");
+const { sleep } = require("./utils");
 
+const nativeTokenTransfer = async (context, ee, next) => {
+  const targetUrl = context.vars.target;
+  const privateKey = context.vars.privateKey;
 
- const nativeTokenTransfer = async (context, ee, next) => {
-    const targetUrl = context.vars.target;
-    const privateKey = context.vars.privateKey;
+  const provider = new providers.JsonRpcProvider(targetUrl);
 
-    const provider = new providers.JsonRpcProvider(targetUrl);
+  const wallet = new Wallet(privateKey, provider);
+  const wallet2 = Wallet.createRandom();
 
-    const wallet = new Wallet(privateKey, provider);
-    const wallet2 = Wallet.createRandom();
+  const recepientAddress = wallet2.address;
+  let nonce = await wallet.getTransactionCount();
 
-    const recepientAddress = wallet2.address;
-    let nonce = await wallet.getTransactionCount();
-
-    try {
-    for (let i =0 ; i < 500 ; i +=1 ) {
-
-    let tx = {
+    for (let i = 0; i < 500; i += 1) {
+      let tx = {
         to: recepientAddress,
-        value: parseEther('0.00001'),
-        nonce
+        value: parseEther("0.00001"),
+        nonce,
+      };
+
+      const populatedTx = await wallet.populateTransaction(tx);
+      const signedTx = await wallet.signTransaction(populatedTx);
+
+      provider
+        .sendTransaction(signedTx)
+        .then()
+        .catch((e) => {
+          // console.log('e ---->', e);
+        });
+
+      nonce += 1;
+      console.log("transaction sent, current nonce ---->", nonce);
+
+      // break to make sure transactions arrive in order to RPC.
+      // NOTE: reduce it if you are skipping validate
+      await sleep(350);
     }
 
-    const populatedTx = await wallet.populateTransaction(tx);
-    const signedTx = await wallet.signTransaction(populatedTx);
-
-    provider.sendTransaction(signedTx).then().catch((e)=>{
-        // console.log('e ---->', e);
-    })
-
-    nonce +=1;
-    console.log("transaction sent, current nonce ---->", nonce);
-
-    // break to make sure transactions arrive in order to RPC.
-    // NOTE: reduce it if you are skipping validate
-    await sleep(350);
-}
-}
-catch(err) {
-    // the rpc is killed once metrics are collected, and hence we will land to this error
-    if(err.code === "NETWORK_ERROR")
-    {
-       console.log("kakarot rpc is down");
-       process.exit(0);
-    }
-    else {
-        throw err
-    }
-}
-
-    next();
-}
+  next();
+};
 
 module.exports = {
-    nativeTokenTransfer,
-}
+  nativeTokenTransfer,
+};
