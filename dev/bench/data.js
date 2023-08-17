c009af105fc8b73407f2bb01315578b1aafa76f9
github-action-benchmark <github@users.noreply.github.com>
2023-08-17 10:34:30 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 6d2d12021355fcecebaf65c775e3f6038a8a564d

---
+++ dev/bench/data.js
@@ -0,0 +1,41 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1692268470448,
+  "repoUrl": "https://github.com/bajpai244/kakarot-rpc",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6d2d12021355fcecebaf65c775e3f6038a8a564d",
+          "message": "build(deps): bump lib/madara from `327ac7f` to `d4f5404` (#443)\n\nBumps [lib/madara](https://github.com/keep-starknet-strange/madara) from `327ac7f` to `d4f5404`.\n- [Release notes](https://github.com/keep-starknet-strange/madara/releases)\n- [Commits](https://github.com/keep-starknet-strange/madara/compare/327ac7ff6654049cb5eb2bd5d20b10272590287e...d4f540490edd025edefb83cb6b4f801e6c30ad7e)\n\n---\nupdated-dependencies:\n- dependency-name: lib/madara\n  dependency-type: direct:production\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-08-17T06:39:16Z",
+          "tree_id": "b2f7201009ce27304a88f59271355d30977ede05",
+          "url": "https://github.com/bajpai244/kakarot-rpc/commit/6d2d12021355fcecebaf65c775e3f6038a8a564d"
+        },
+        "date": 1692268462237,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "transactions per block",
+            "value": "transactions/block"
+          },
+          {
+            "name": "transactions per second",
+            "value": 0.48148148148148145,
+            "unit": "transactions/second"
+          }
+        ]
+      }
+    ]
+  }
+}
